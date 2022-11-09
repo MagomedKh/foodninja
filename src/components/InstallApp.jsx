7ff17747a5f87686c633307e6af2e2bb1a2bbe8c
BestMn <maksimezh@gmail.com>
2022-11-09 10:54:59 +0500

    InstallApp после закрытия не предлагаем 1 день

--- src/components/InstallApp.jsx
+++ src/components/InstallApp.jsx
@@ -1,49 +1,72 @@
-import { Drawer } from '@mui/material';
-import * as React from 'react';
-import { useSelector } from 'react-redux';
+import * as React from "react";
+import { useSelector } from "react-redux";
+import { Drawer } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import { _getMobileType } from "./helpers";
+import { addDays } from "date-fns";
+import Cookies from "universal-cookie";
 import "../css/install-app.css";
-import { _getMobileType } from './helpers';
-import CloseIcon from '@mui/icons-material/Close';
 
+export default function InstallApp({ initStatus = false }) {
+    const cookies = new Cookies();
 
-export default function InstallApp( {initStatus = false} ) {
-
-    const {icon, linkIos, linkAndroid, installText} = useSelector( ({config}) => {
-        return {
-            icon: config.data.CONFIG_company_favicon,
-            linkIos: config.data.CONFIG_APPSTORE,
-            linkAndroid: config.data.CONFIG_GPLAY,
-            installText: config.data.CONFIG_mobile_install_text
+    const { icon, linkIos, linkAndroid, installText } = useSelector(
+        ({ config }) => {
+            return {
+                icon: config.data.CONFIG_company_favicon,
+                linkIos: config.data.CONFIG_APPSTORE,
+                linkAndroid: config.data.CONFIG_GPLAY,
+                installText: config.data.CONFIG_mobile_install_text,
+            };
         }
-    });
+    );
 
     let [open, setOpen] = React.useState(true);
     const mobileType = _getMobileType();
 
-    const handleClose = () => {
-        setOpen(!open);
+    const refusedToInstallApp = cookies.get("refusedToInstallApp");
+
+    if (refusedToInstallApp) {
+        return null;
     }
+
+    const handleClose = () => {
+        cookies.set("refusedToInstallApp", "true", {
+            path: "/",
+            expires: addDays(new Date(), 1),
+        });
+        setOpen(!open);
+    };
     let linkApp;
 
-    if( mobileType == 'android' && linkAndroid ) {
+    if (mobileType == "android" && linkAndroid) {
         linkApp = linkAndroid;
-    } else if( mobileType == 'ios' && linkIos ) {
+    } else if (mobileType == "ios" && linkIos) {
         linkApp = linkIos;
     } else open = false;
 
     return (
         <React.Fragment key="bottom">
-            <Drawer            
+            <Drawer
                 anchor="bottom"
                 open={open}
                 onClose={handleClose}
-                className="install-app--panel">
+                className="install-app--panel"
+            >
                 <div className="install-app--inner">
                     <img className="install-app--icon" src={icon} />
-                    <h4 className="install-app--title">Скачай мобильное приложение</h4>
-                    <CloseIcon className="install-app--close" onClick={handleClose}/>
-                    { installText ? (
-                        <div className="install-app--text" dangerouslySetInnerHTML={{__html: installText}}></div>
+                    <h4 className="install-app--title">
+                        Скачай мобильное приложение
+                    </h4>
+                    <CloseIcon
+                        className="install-app--close"
+                        onClick={handleClose}
+                    />
+                    {installText ? (
+                        <div
+                            className="install-app--text"
+                            dangerouslySetInnerHTML={{ __html: installText }}
+                        ></div>
                     ) : (
                         <div className="install-app--text">
                             <ul>
@@ -52,8 +75,14 @@ export default function InstallApp( {initStatus = false} ) {
                                 <li>Получай персональные скидки и акции</li>
                             </ul>
                         </div>
-                    ) }
-                    <a href={linkApp} target="_blank" className="btn btn-lg btn--action w-100 t-center">Установить приложение</a>
+                    )}
+                    <a
+                        href={linkApp}
+                        target="_blank"
+                        className="btn btn-lg btn--action w-100 t-center"
+                    >
+                        Установить приложение
+                    </a>
                 </div>
             </Drawer>
         </React.Fragment>
