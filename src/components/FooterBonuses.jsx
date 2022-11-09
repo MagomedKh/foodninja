e9c7f63eedf6492c81bfb2a9335a5aa4c9196791
BestMn <maksimezh@gmail.com>
2022-11-09 09:41:50 +0500

    В модалке выбора подарков доп.строка для названия При выборе подарка закрываем модалку

--- src/components/FooterBonuses.jsx
+++ src/components/FooterBonuses.jsx
@@ -61,6 +61,7 @@ export default function BonusesProductsModal() {
 
     const handleChooseBonusProduct = (item) => {
         dispatch(addBonusProductToCart(item));
+        dispatch(setOpenBonusesModal(false));
     };
 
     const handleBonusesHandler = () => {
