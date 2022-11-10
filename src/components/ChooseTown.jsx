9e0c9a6b7c09c9f30f0ec0817b9f607c85cbf1d7
BestMn <maksimezh@gmail.com>
2022-11-10 12:59:42 +0500

    Проверка если выбранный город = базовому домену

--- src/components/ChooseTown.jsx
+++ src/components/ChooseTown.jsx
@@ -38,6 +38,7 @@ export default function ChooseTown() {
         if (
             config.status &&
             _getDomain() === config.data.baseDomain &&
+            config.data.baseDomain !== currentTown &&
             (!currentTown || config.data.CONFIG_always_choose_town === "on")
         ) {
             dispatch(setTownModal(true));
@@ -70,7 +71,7 @@ export default function ChooseTown() {
                       {value.name}
                   </a>
               </div>
-        ));
+          ));
 
     const handleAlertClose = () => {
         dispatch(setTownModal(false));
