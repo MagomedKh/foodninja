ad18bfbb915a5681fe36bd60472ff29abe73a3af
BestMn <maksimezh@gmail.com>
2023-03-10 11:02:45 +0500

    Выбор лоадера

--- src/components/BigLoader.jsx
+++ src/components/BigLoader.jsx
@@ -1,5 +1,10 @@
 import * as React from "react";
 import CircularProgress from "@mui/material/CircularProgress";
+import { ReactComponent as TypingLoader } from "../img/Typing.svg";
+import { ReactComponent as BricksLoader } from "../img/Bricks.svg";
+import { ReactComponent as EaterLoader } from "../img/Eater.svg";
+import { ReactComponent as RippleLoader } from "../img/Ripple.svg";
+import { ReactComponent as ProgressLoader } from "../img/Progress.svg";
 import "../css/loader.css";
 
 export default function BigLoader({ initStatus = false }) {
@@ -9,81 +14,17 @@ export default function BigLoader({ initStatus = false }) {
         <div>
             <div className="mainloader">
                 <div className="loader-inner">
-                    <svg
-                        width="150px"
-                        height="150px"
-                        viewBox="0 0 100 100"
-                        preserveAspectRatio="xMidYMid"
-                    >
-                        <circle
-                            cx="27.5"
-                            cy="57.5"
-                            r="5"
-                            fill={color || "#fe718d"}
-                        >
-                            <animate
-                                attributeName="cy"
-                                calcMode="spline"
-                                keySplines="0 0.5 0.5 1;0.5 0 1 0.5;0.5 0.5 0.5 0.5"
-                                repeatCount="indefinite"
-                                values="57.5;42.5;57.5;57.5"
-                                keyTimes="0;0.3;0.6;1"
-                                dur="1s"
-                                begin="-0.6s"
-                            ></animate>
-                        </circle>{" "}
-                        <circle
-                            cx="42.5"
-                            cy="57.5"
-                            r="5"
-                            fill={color || "#f47e60"}
-                        >
-                            <animate
-                                attributeName="cy"
-                                calcMode="spline"
-                                keySplines="0 0.5 0.5 1;0.5 0 1 0.5;0.5 0.5 0.5 0.5"
-                                repeatCount="indefinite"
-                                values="57.5;42.5;57.5;57.5"
-                                keyTimes="0;0.3;0.6;1"
-                                dur="1s"
-                                begin="-0.44999999999999996s"
-                            ></animate>
-                        </circle>{" "}
-                        <circle
-                            cx="57.5"
-                            cy="57.5"
-                            r="5"
-                            fill={color || "#f8b26a"}
-                        >
-                            <animate
-                                attributeName="cy"
-                                calcMode="spline"
-                                keySplines="0 0.5 0.5 1;0.5 0 1 0.5;0.5 0.5 0.5 0.5"
-                                repeatCount="indefinite"
-                                values="57.5;42.5;57.5;57.5"
-                                keyTimes="0;0.3;0.6;1"
-                                dur="1s"
-                                begin="-0.3s"
-                            ></animate>
-                        </circle>{" "}
-                        <circle
-                            cx="72.5"
-                            cy="57.5"
-                            r="5"
-                            fill={color || "#abbd81"}
-                        >
-                            <animate
-                                attributeName="cy"
-                                calcMode="spline"
-                                keySplines="0 0.5 0.5 1;0.5 0 1 0.5;0.5 0.5 0.5 0.5"
-                                repeatCount="indefinite"
-                                values="57.5;42.5;57.5;57.5"
-                                keyTimes="0;0.3;0.6;1"
-                                dur="1s"
-                                begin="-0.15s"
-                            ></animate>
-                        </circle>
-                    </svg>
+                    {window.typeLoader === "bricks" ? (
+                        <BricksLoader />
+                    ) : window.typeLoader === "eater" ? (
+                        <EaterLoader />
+                    ) : window.typeLoader === "ripple" ? (
+                        <RippleLoader />
+                    ) : window.typeLoader === "progress" ? (
+                        <ProgressLoader />
+                    ) : (
+                        <TypingLoader />
+                    )}
                 </div>
             </div>
         </div>
