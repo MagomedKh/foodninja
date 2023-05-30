5f3b8c9bc4ab42130decceb9cf197158802bfbf4
BestMn <maksimezh@gmail.com>
2023-05-30 10:44:26 +0500

    Верстка инфо-блоков зон доставки в контактах

--- src/components/ContactsZonesInfo.jsx
+++ src/components/ContactsZonesInfo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Divider } from "@mui/material";
+import { Divider, Grid } from "@mui/material";
 
 const ContactsZonesInfo = () => {
     const zones = useSelector(({ config }) => config.data.deliveryZones?.zones);
@@ -19,72 +19,77 @@ const ContactsZonesInfo = () => {
                     }
                     return (
                         <div className="contacts-zones-info--zone">
-                            <div className="zone-header">
-                                <div
-                                    className="zone-color"
-                                    style={{ backgroundColor: zone.fillColor }}
-                                />
-                                <div className="zone-title">
-                                    <span>{zone.name}</span>
-                                    {zone.deliveryTime ? (
-                                        <span>
-                                            , доставка от {zone.deliveryTime}{" "}
-                                            мин.
-                                        </span>
+                            <div className="zone-inner-wrapper">
+                                <div className="zone-header">
+                                    <div
+                                        className="zone-color"
+                                        style={{
+                                            backgroundColor: zone.fillColor,
+                                        }}
+                                    />
+                                    <div className="zone-title">
+                                        <span>{zone.name}</span>
+                                        {zone.deliveryTime ? (
+                                            <span>
+                                                , доставка от{" "}
+                                                {zone.deliveryTime} мин.
+                                            </span>
+                                        ) : null}
+                                    </div>
+                                </div>
+                                <div className="zone-body">
+                                    {zone.orderMinPrice ? (
+                                        <div className="zone-body--col">
+                                            <div className="zone-body--col-title">
+                                                Минимальный заказ
+                                            </div>
+                                            <div className="zone-body--col-value">
+                                                {zone.orderMinPrice} ₽
+                                            </div>
+                                        </div>
+                                    ) : null}
+                                    {zone.deliveryPrice ? (
+                                        <>
+                                            <Divider
+                                                orientation="vertical"
+                                                flexItem
+                                                sx={{
+                                                    borderWidth: "1px",
+                                                    m: "5px 16px 4px 5px",
+                                                }}
+                                            />
+                                            <div className="zone-body--col">
+                                                <div className="zone-body--col-title">
+                                                    Стоимость доставки
+                                                </div>
+                                                <div className="zone-body--col-value">
+                                                    {zone.deliveryPrice} ₽
+                                                </div>
+                                            </div>
+                                        </>
+                                    ) : null}
+                                    {zone.freeDeliveryOrder ? (
+                                        <>
+                                            <Divider
+                                                orientation="vertical"
+                                                flexItem
+                                                sx={{
+                                                    borderWidth: "1px",
+                                                    m: "16px 16px 4px 16px",
+                                                }}
+                                            />
+                                            <div className="zone-body--col">
+                                                <div className="zone-body--col-title">
+                                                    Бесплатная доставка
+                                                </div>
+                                                <div className="zone-body--col-value">
+                                                    от {zone.freeDeliveryOrder}{" "}
+                                                    ₽
+                                                </div>
+                                            </div>
+                                        </>
                                     ) : null}
                                 </div>
-                            </div>
-                            <div className="zone-body">
-                                {zone.orderMinPrice ? (
-                                    <div className="zone-body--col">
-                                        <div className="zone-body--col-title">
-                                            Минимальный заказ
-                                        </div>
-                                        <div className="zone-body--col-value">
-                                            {zone.orderMinPrice} ₽
-                                        </div>
-                                    </div>
-                                ) : null}
-                                {zone.deliveryPrice ? (
-                                    <>
-                                        <Divider
-                                            orientation="vertical"
-                                            flexItem
-                                            sx={{
-                                                borderWidth: "1px",
-                                                m: "16px 16px 4px 16px",
-                                            }}
-                                        />
-                                        <div className="zone-body--col">
-                                            <div className="zone-body--col-title">
-                                                Стоимость доставки
-                                            </div>
-                                            <div className="zone-body--col-value">
-                                                {zone.deliveryPrice} ₽
-                                            </div>
-                                        </div>
-                                    </>
-                                ) : null}
-                                {zone.freeDeliveryOrder ? (
-                                    <>
-                                        <Divider
-                                            orientation="vertical"
-                                            flexItem
-                                            sx={{
-                                                borderWidth: "1px",
-                                                m: "16px 16px 4px 16px",
-                                            }}
-                                        />
-                                        <div className="zone-body--col">
-                                            <div className="zone-body--col-title">
-                                                Бесплатная доставка
-                                            </div>
-                                            <div className="zone-body--col-value">
-                                                от {zone.freeDeliveryOrder} ₽
-                                            </div>
-                                        </div>
-                                    </>
-                                ) : null}
                             </div>
                         </div>
                     );
